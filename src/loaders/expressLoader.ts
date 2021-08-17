4abc61d3a06ced824ecebe19d30cd7dc878c5bc7
guruhedipurwanto <guruhedipurwanto@gmail.com>
2021-08-17 18:53:57 +0800

    add swagger ui for documentation

--- src/loaders/expressLoader.ts
+++ src/loaders/expressLoader.ts
@@ -1,12 +1,35 @@
 import { Application } from 'express';
 import express from 'express';
-import { useExpressServer } from 'routing-controllers';
+import {
+  getMetadataArgsStorage,
+  RoutingControllersOptions,
+  useExpressServer,
+} from 'routing-controllers';
 import path from 'path';
+import { routingControllersToSpec } from 'routing-controllers-openapi';
+import swaggerUI from 'swagger-ui-express';
 
 export const expressLoader = (): Application => {
   const app = express();
-  useExpressServer(app, {
+
+  const myOptions: RoutingControllersOptions = {
+    routePrefix: '/api',
     controllers: [path.join(__dirname, '..', 'controllers', '*.js')],
+  };
+
+  const spec = routingControllersToSpec(getMetadataArgsStorage(), myOptions, {
+    info: {
+      title: 'Simple Shop Service',
+      version: '1.0.0',
+      license: {
+        name: 'MIT',
+      },
+    },
   });
+
+  app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(spec));
+
+  useExpressServer(app, myOptions);
+
   return app;
 };
